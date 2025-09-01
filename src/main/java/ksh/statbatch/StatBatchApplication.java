cbce28ac50500ced178214e127740015dc220737
kim-se-hee <saree98@naver.com>
2025-09-01 17:20:21 +0900

    init: 최초 커밋

---
+++ src/main/java/ksh/statbatch/StatBatchApplication.java
@@ -0,0 +1,13 @@
+package ksh.statbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StatBatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ksh/statbatch/StatBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package ksh.statbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
