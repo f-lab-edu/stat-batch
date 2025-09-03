16dbe534a22defa016147e01116863e7f601cb5f
kim-se-hee <saree98@naver.com>
2025-09-03 14:30:16 +0900

    feat: 통계 데이터 갱신을 위한 Job과 Step 등록

---
+++ src/main/java/ksh/statbatch/quiz/batch/job/config/WrongQuizMonthlyStatJopConfig.java
@@ -0,0 +1,55 @@
+package ksh.statbatch.quiz.batch.job.config;
+
+import ksh.statbatch.quiz.batch.reader.DailyAggregationInMemoryReader;
+import ksh.statbatch.quiz.batch.writer.MonthlyAggregationUpsertWriter;
+import ksh.statbatch.quiz.dto.DailyAggregation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import java.util.List;
+
+@Configuration
+@EnableBatchProcessing
+@RequiredArgsConstructor
+public class WrongQuizMonthlyStatJopConfig {
+
+    private final JobRepository jobRepository;
+    private final PlatformTransactionManager transactionManager;
+
+    @Bean
+    public Job wrongQuizDailyAccumulateJob(
+        Step dailyAccumulateStep
+    ) {
+        return new JobBuilder("wrong-quiz-daily-accumulate-job", jobRepository)
+            .incrementer(new RunIdIncrementer())
+            .validator(params -> {
+                if (!params.getParameters().containsKey("aggregationDay")) {
+                    throw new JobParametersInvalidException("Missing param: aggregationDay");
+                }
+            })
+            .start(dailyAccumulateStep)
+            .build();
+    }
+
+    @Bean
+    public Step dailyAccumulateStep(
+        DailyAggregationInMemoryReader reader,
+        MonthlyAggregationUpsertWriter writer
+    ) {
+        return new StepBuilder("daily-accumulate-step", jobRepository)
+            .<DailyAggregation, DailyAggregation>chunk(10000, transactionManager)
+            .reader(reader)
+            .writer(writer)
+            .build();
+    }
+}
