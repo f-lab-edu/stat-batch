19a82abc0077a7e048d9b381084100688b99444e
kim-se-hee <saree98@naver.com>
2025-09-08 17:58:33 +0900

    refactor: Writer에서 SQL 문을 분리

--- src/main/java/ksh/statbatch/quiz/batch/writer/MonthlyAggregationUpsertWriter.java
+++ src/main/java/ksh/statbatch/quiz/batch/writer/MonthlyAggregationUpsertWriter.java
@@ -1,38 +1,25 @@
 package ksh.statbatch.quiz.batch.writer;
 
-import jakarta.annotation.PostConstruct;
 import ksh.statbatch.quiz.dto.DailySongAggregation;
+import ksh.statbatch.quiz.repository.WrongQuizMonthlyStatRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.configuration.annotation.StepScope;
-import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
-import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.Chunk;
+import org.springframework.batch.item.ItemWriter;
 import org.springframework.stereotype.Component;
 
-import javax.sql.DataSource;
+import java.util.List;
 
 @Component
 @StepScope
 @RequiredArgsConstructor
-public class MonthlyAggregationUpsertWriter extends JdbcBatchItemWriter<DailySongAggregation> {
+public class MonthlyAggregationUpsertWriter implements ItemWriter<DailySongAggregation> {
 
-    private static final String SQL = """
-        INSERT INTO wrong_quiz_monthly_stat
-            (base_date, song_id, wrong_count, total_tries, wrong_rate)
-        VALUES
-            (:baseDate, :songId, :wrongInc, :triesInc, ROUND(:wrongInc * 1.0 / :triesInc, 4))
-        ON DUPLICATE KEY UPDATE
-            wrong_count = wrong_quiz_monthly_stat.wrong_count + VALUES(wrong_count),
-            total_tries = wrong_quiz_monthly_stat.total_tries + VALUES(total_tries),
-            wrong_rate  = ROUND(wrong_count * 1.0 / total_tries, 4);
-        """;
+    private final WrongQuizMonthlyStatRepository wrongQuizMonthlyStatJdbcRepository;
 
-    private final DataSource dataSource;
-
-    @PostConstruct
-    public void init() {
-        setDataSource(dataSource);
-        setItemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>());
-        setSql(SQL);
-        afterPropertiesSet();
+    @Override
+    public void write(Chunk<? extends DailySongAggregation> chunk) throws Exception {
+        List<DailySongAggregation> items = (List<DailySongAggregation>) chunk.getItems();
+        wrongQuizMonthlyStatJdbcRepository.upsertAll(items);
     }
 }
