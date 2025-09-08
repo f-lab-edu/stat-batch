b03b454f611e1d9eeb1846cb242bdab74d2064a1
kim-se-hee <saree98@naver.com>
2025-09-08 10:30:10 +0900

    feat: 특정 일자의 퀴즈 풀이 결과 조회 메소드 구현

--- src/main/java/ksh/statbatch/quiz/repository/QuizAttemptHistoryRepository.java
+++ src/main/java/ksh/statbatch/quiz/repository/QuizAttemptHistoryRepository.java
@@ -3,5 +3,5 @@ package ksh.statbatch.quiz.repository;
 import ksh.statbatch.quiz.entity.QuizAttemptHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface QuizAttemptHistoryRepository extends JpaRepository<QuizAttemptHistory, Long> {
+public interface QuizAttemptHistoryRepository extends JpaRepository<QuizAttemptHistory, Long>, QuizAttemptHistoryQueryRepository {
 }
